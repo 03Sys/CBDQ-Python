c47d731796f27020a5c224b35086632c569789e4
Ziad <94720757+03Sys@users.noreply.github.com>
2025-03-23 21:48:53 +0200

    Update

--- src/main.py
+++ src/main.py
@@ -1,155 +1,248 @@
-from tracery import Tracery
-import sys
+import os, json, tweepy, time, random, requests, re, sys
+import keep_alive  # at the top
 
-try:
-    import requests
-except:
-    print("You need to install requests. Try running 'pip install requests' in your terminal.")
-    sys.exit(1)
 
-try:
-    import tweepy
-except:
-    print("You need to install tweepy. Try running 'pip install tweepy' in your terminal.")
-    sys.exit(1)
+keep_alive.keep_alive()
 
-import os, sys, time, re, random
-from dotenv import load_dotenv
-random.seed(time.time())
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
+consumer_key = ""
+consumer_secret = ""
+access_token = ""
+access_token_secret = ""
+bearer_token = ""
 
-JsonSource = Tracery("bot.json")
+cur_version = "1.0.6"
 
-TimeBetweenTweets = 60 * 60 * 0.5 # 30 minutes. 60 seconds * 60 minutes * 0.5 hours
-
-load_dotenv(".env")
-
-Client = tweepy.Client(
-    consumer_key= os.environ["consumer_key"],
-    consumer_secret= os.environ["consumer_secret"],
-    access_token= os.environ["access_token"],
-    access_token_secret= os.environ["access_token_secret"],
-    bearer_token= os.environ["bearer_token"]
+# Use twitter api V2
+Client = tweepy.Client(bearer_token=bearer_token, 
+                       consumer_key=consumer_key, 
+                       consumer_secret=consumer_secret, 
+                       access_token=access_token, 
+                       access_token_secret=access_token_secret
 )
-
-auth = tweepy.OAuth1UserHandler(
-    consumer_key= os.environ["consumer_key"],
-    consumer_secret= os.environ["consumer_secret"],
-    access_token= os.environ["access_token"],
-    access_token_secret= os.environ["access_token_secret"]
+auth = tweepy.OAuth1UserHandler(consumer_key,
+                                consumer_secret,
+                                access_token,
+                                access_token_secret
 )
-
+# for media upload
 api = tweepy.API(auth, wait_on_rate_limit=True)
 
+# Verify Credentials
 try:
     api.verify_credentials()
-    print("Authentication OK")
+    print("Authenticated.")
 except:
-    print("Error during authentication")
+    print("Error during authentication.")
+    sys.exit()
 
-try:
-    requestsVer = requests.__version__
-    requestsCurrentVersion = requests.get("https://pypi.org/pypi/requests/json").json()["info"]["version"]
-    if requestsVer != requestsCurrentVersion:
-        print("You should update requests. Try running 'pip install requests --upgrade' in your terminal.")
-except:
-    print("Couldn't check for requests update. Please check manually with 'pip show requests'.")
-    pass
+# The time between every new tweet
+# This is in seconds, so 3600 is 1 hour
+time_between_tweets = 3600
 
-global mediaIDs, mediaList
+# read bot.json
+botjson = open("bot.json", "r", encoding="utf-8")
+botjson = json.load(botjson)
 
-ENVIROMENT = {}
-
+global blahList
+global mediaIDs
+global imgList
+global otherList
+global videoList
+blahList = []
 mediaIDs = []
-mediaList = []
+imgList = []
+otherList = []
+videoList = []
 
-def ResetLists():
-    global mediaIDs, mediaList
-    mediaIDs = []
-    mediaList = []
-    ENVIROMENT = {}
+def resetLists():
+    blahList.clear()
+    mediaIDs.clear()
+    imgList.clear()
+    otherList.clear()
+    videoList.clear()
 
-def GenerateTweet():
-    ResetLists()
-    tweet = JsonSource.GetMainRule()
+def generateTweet():
+    tweet = random.choice(botjson["origin"])
+    resetLists()
 
+    # get all the #blah# in the tweet,can also be given as a lowercase, uppercase, numbers, special characters, spaces, etc
+    for blah in re.findall(r"#[a-zA-Z0-9]+#", tweet):
+        blahList.append(blah)
+
+    # replace the #blah# with a random word from the blah array
+    for blah in blahList:
+        choice = random.choice(botjson[blah[1:-1]])
+        
+        while choice in otherList:
+            choice = random.choice(botjson[blah[1:-1]])
+        otherList.append(choice)
+
+        tweet = tweet.replace(blah, choice, 1)
+        # check if theres another #blah# in the tweet, can be character, item, etc
+        if re.findall(r"#[a-zA-Z0-9]+#", tweet):
+            for blah in re.findall(r"#[a-zA-Z0-9]+#", tweet):
+                choice = random.choice(botjson[blah[1:-1]])
+                
+                while choice in otherList:
+                    choice = random.choice(botjson[blah[1:-1]])
+                otherList.append(choice)
+
+                tweet = tweet.replace(blah, choice, 1)
+        #print(f"Replaced {blah} with {choice}")
+
+    # get all the {img link} in the tweet
     for img in re.findall(r"{img \S+}", tweet):
-        # remove {img \S+}
-        print("Found image: " + img)
+        imgList.append(img)
+        # remove it from the tweet
         tweet = tweet.replace(img, "")
-        # remove {img}
-        img = img.replace("{img ", "").replace("}", "").strip()
-        # add to mediaList
-        mediaList.append(img)
-        
-    for video in re.findall(r"{vid \S+}", tweet):
-        # remove {vid \S+}
-        tweet = tweet.replace(video, "")
-        # remove {vid}
-        video = video.replace("{vid ", "").replace("}", "").strip()
-        # add to mediaList
-        mediaList.append(video)
-        
 
-    for num in re.findall(r"{rand \d+, \d+}", tweet):
-        num = num.replace("{rand ", "").replace("}", "").strip()
-        num1, num2 = num.split(",")
-        tweet = tweet.replace("{rand " + num + "}", str(random.randint(int(num1), int(num2))))
+    # get all the {vid link} in the tweet
+    for vid in re.findall(r"{vid \S+}", tweet):
+        videoList.append(vid)
+        # remove it from the tweet
+        tweet = tweet.replace(vid, "")
 
-    # arg 1: rule, arg 2: amount to generate from JsonSource, e.g. {StoreVariable, random thank you!, 3, bool}
-    for store in re.findall(r"{StoreVariable, [a-zA-Z0-9 ]+!, \d+, \w+}", tweet):
-        bs = store
-        store = store.replace("{StoreVariable, ", "").replace("}", "").strip()
-        rule, amount, boolean = store.split(", ")
-        if rule not in ENVIROMENT:
-            ENVIROMENT[rule] = []
-        for i in range(int(amount)):
-            ENVIROMENT[rule].append(JsonSource.GetRule(f"#{rule}#", isStored=True, isStoredBoolean=(boolean.lower() == "true")))
-            
-        tweet = tweet.replace(bs, "")
-            
-        #print("Stored " + str(amount) + " " + rule + "s", ENVIROMENT)
+    for img in imgList:
+        # download the image w/ requests
+        image = img.split("{img ")[1]
+        image = image.split("}")[0]
+        image_name = image.split("/")[-1]
+        # sometimes the image link can have a query string, so we need to remove it
+        if "?" in image_name:
+            image_name = image_name.split("?")[0]
 
-    # GetVariable, arg 1: rule, arg 2: index of variable, e.g. {GetVariable, thank you!, 0}
-    for get in re.findall(r"{GetVariable, [a-zA-Z0-9 ]+!, \d+}", tweet):
-        bg = get
-        get = get.replace("{GetVariable, ", "").replace("}", "").strip()
-        rule, index = get.split(", ")
-        tweet = tweet.replace(bg, ENVIROMENT[rule][int(index)])
+        r = requests.get(image, allow_redirects=True)
+        try:
+            try:
+                open(image_name, "wb").write(r.content)
+            except:
+                image_name = "temp.png"
+                open("temp.png", "wb").write(r.content)
+        except:
+            image_name = "unknown.png"
 
-        #print("Got " + rule + " at index " + index, ENVIROMENT)
+        # upload the image to twitter
+        try:
+            media = api.media_upload(image_name)
+        except:
+            image_name = "unknown.png"
+            media = api.media_upload(image_name)
+        print(f"Uploaded image: {image_name}")
+        mediaIDs.append(media.media_id)
 
-    for media in mediaList:
-        mediaIDs.append(UploadMedia(media))
+        # replace the {img link} with nothing
+        tweet = tweet.replace(img, "")
 
-    if len(mediaIDs) > 0:
-        Client.create_tweet(text=tweet, media_ids=mediaIDs)
-    else:
-        Client.create_tweet(text=tweet)
+    for vid in videoList:
+        # download the video w/ requests
+        video = vid.split("{vid ")[1]
+        video = video.split("}")[0]
+        video_name = video.split("/")[-1]
+        # sometimes the video link can have a query string, so we need to remove it
+        if "?" in video_name:
+            video_name = video_name.split("?")[0]
+        r = requests.get(video, allow_redirects=True)
+        try:
+            try:
+                open(video_name, "wb").write(r.content)
+            except:
+                video_name = "temp.mp4"
+                open("temp.mp4", "wb").write(r.content)
+        except:
+            video_name = "unknown.mp4"
 
-    print("Tweeted: " + tweet)
+        # upload the video to twitter
+        media = api.media_upload(video_name)
+        print(f"Uploaded video: {video_name}")
+        mediaIDs.append(media.media_id)
 
+        # replace the {vid link} with nothing
+        tweet = tweet.replace(vid, "")
 
-def UploadMedia(media):
-    # remove media if exists
-    if os.path.exists("media"):
-        os.remove("media")
-    # download media
-    try:
-        r = requests.get(media, allow_redirects=True)
-        open("media", "wb").write(r.content)
-    except:
-        # upload unknown.png 
-        open("media", "wb").write(open("unknown.png", "rb").read())
-    media = api.media_upload("media")
-    return media.media_id
-
+    return tweet
 now = 0
+while True:
+    timer = time.time()
+    
+    #print(f"Time since last tweet: {timer - (now or 0)}")
+    if timer - (now or 0) >= time_between_tweets:
+        try:
+            print("Tweeting...")
+        except:
+            pass
 
-if __name__ == "__main__":
-    while True:
-        if time.time() - (now or 0) >= TimeBetweenTweets:
-            GenerateTweet()
-            now = time.time()
-        else:
-            time.sleep(1)
+        # get a random tweet from the origin array
+        tweet = generateTweet()
+
+        # tweet the tweet
+        try:
+            try:
+                if len(mediaIDs) == 0:
+                    Client.create_tweet(text=tweet)
+                else:
+                    Client.create_tweet(text=tweet, media_ids=mediaIDs)
+            except:
+                # keep generating tweets until it works
+                while True:
+                    tweet = generateTweet()
+                    try:
+                        Client.create_tweet(text=tweet, media_ids=mediaIDs)
+                        break
+                    except:
+                        # if error is <tweepy.errors.BadRequest>
+                        # print possible error
+                        print("Error: ", sys.exc_info()[0])
+                        if sys.exc_info()[0] == tweepy.errors.BadRequest:
+                            print("""
+Possible error!
+    1. Tweet is too long.
+    2. Tweet is a duplicate.
+    3. API key is invalid.
+                            """)
+                            pass
+                        elif sys.exc_info()[0] == tweepy.errors.Forbidden:
+                            print("""
+Possible error!
+    1. API key doesn't have permission to tweet | check if it's read-only.
+                            """)
+                            sys.exit()
+            print(f"Tweeted: {tweet}")
+        except:
+            print(f"Tweet failed: {tweet}")
+            # print the error
+            print(sys.exc_info()[0])
+
+        # delete the images
+        for image in imgList:
+            image = image.split("{img ")[1]
+            image = image.split("}")[0]
+            image_name = image.split("/")[-1]
+            # sometimes the image link can have a query string, so we need to remove it
+            if "?" in image_name:
+                image_name = image_name.split("?")[0]
+            try:
+                os.remove(image_name)
+            except:
+                try:
+                    os.remove("temp.png")
+                except:
+                    print("Couldn't delete image; it probably doesn't exist.")
+
+        # delete the videos
+        for video in videoList:
+            video = video.split("{vid ")[1]
+            video = video.split("}")[0]
+            video_name = video.split("/")[-1]
+            # sometimes the video link can have a query string, so we need to remove it
+            if "?" in video_name:
+                video_name = video_name.split("?")[0]
+            try:
+                os.remove(video_name)
+            except:
+                try:
+                    os.remove("temp.mp4")
+                except:
+                    print("Couldn't delete video; it probably doesn't exist.")
+        now = time.time()
+    else:
+        time.sleep(1)
